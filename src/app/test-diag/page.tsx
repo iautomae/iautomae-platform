a6599ade445ad70014fd1751c97c7fdaa84d2484
yelmorp1 <yelmorp@gmail.com>
2026-02-06 20:15:46 -0500

    debug: add test route and revert build command

---
+++ src/app/test-diag/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: 50, background: 'white', color: 'black' }}>
+            <h1>Test Route Works</h1>
+            <p>If you see this, routing is working for subpaths.</p>
+        </div>
+    )
+}
